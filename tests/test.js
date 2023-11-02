a4dda5fe840460f2be577de2155ffa3045986d97
achrafameur <achref.ameur1925@gmail.com>
2023-11-02 16:40:02 +0100

    test fix

---
+++ tests/test.js
@@ -0,0 +1,36 @@
+// tests/test.js
+import { strictEqual } from "assert";
+
+// Fonction d'addition à tester
+function addition(a, b) {
+  return a + b;
+}
+
+// Test 1
+const result1 = addition(2, 3);
+const expected1 = 5;
+strictEqual(
+  result1,
+  expected1,
+  "Le résultat de l'addition (2 + 3) devrait être égal à 5."
+);
+
+// Test 2
+const result2 = addition(-2, -3);
+const expected2 = -5;
+strictEqual(
+  result2,
+  expected2,
+  "Le résultat de l'addition (-2 + -3) devrait être égal à -5."
+);
+
+// Test 3
+const result3 = addition(0, 0);
+const expected3 = 0;
+strictEqual(
+  result3,
+  expected3,
+  "Le résultat de l'addition (0 + 0) devrait être égal à 0."
+);
+
+console.log("Tous les tests ont réussi.");
